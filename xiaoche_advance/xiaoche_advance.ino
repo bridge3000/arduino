59a17b28888489b0f0f005d65f010ee2a07065e9
qiaoliang <qiaoliang@chukong-inc.com>
2016-04-06 17:58:12 +0800

    advance xiaoche

---
+++ xiaoche_advance/xiaoche_advance.ino
@@ -0,0 +1,129 @@
+//============================亚博科技===========================
+//  智能小车前后左右综合实验
+//===============================================================
+//#include <Servo.h> 
+int Left_motor_back=8;     //左电机后退(IN1)
+int Left_motor_go=9;     //左电机前进(IN2)
+
+int Right_motor_go=10;    // 右电机前进(IN3)
+int Right_motor_back=11;    // 右电机后退(IN4)
+int val;
+int speed = 200;
+int acceleration = 10; //加速度
+
+void setup()
+{
+  Serial.begin(9600);//设置波特率为9600
+  Serial.println("servo=o_seral_simple ready" ) ;
+  
+  //初始化电机驱动IO为输出方式
+  pinMode(Left_motor_go,OUTPUT); // PIN 8 (PWM)
+  pinMode(Left_motor_back,OUTPUT); // PIN 9 (PWM)
+  pinMode(Right_motor_go,OUTPUT);// PIN 10 (PWM) 
+  pinMode(Right_motor_back,OUTPUT);// PIN 11 (PWM)
+}
+
+void loop()
+{  
+  val = Serial.read();//读取串口收到的数据
+  if(val == 'go')
+  {
+//    go(100);
+  }
+
+  go();
+  delay(2000);
+}
+
+void go()     // 前进
+{
+  Serial.println("go") ;
+  digitalWrite(Right_motor_go,HIGH);  // 右电机前进
+  digitalWrite(Right_motor_back,LOW);     
+  analogWrite(Right_motor_go,speed);//PWM比例0~255调速，左右轮差异略增减
+  analogWrite(Right_motor_back,0);
+  digitalWrite(Left_motor_go,HIGH);  // 左电机前进
+  digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,speed);//PWM比例0~255调速，左右轮差异略增减
+  analogWrite(Left_motor_back,0);
+}
+
+//刹车，停车
+void brake(int time)         
+{
+  digitalWrite(Right_motor_go,LOW);
+  digitalWrite(Right_motor_back,LOW);
+  digitalWrite(Left_motor_go,LOW);
+  digitalWrite(Left_motor_back,LOW);
+  delay(time * 100);//执行时间，可以调整  
+}
+
+//左转(左轮不动，右轮前进)
+void left(int time)         
+{
+  digitalWrite(Right_motor_go,HIGH);	// 右电机前进
+  digitalWrite(Right_motor_back,LOW);
+  analogWrite(Right_motor_go,200); 
+  analogWrite(Right_motor_back,0);//PWM比例0~255调速
+  digitalWrite(Left_motor_go,LOW);   //左轮不动
+  digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,0); 
+  analogWrite(Left_motor_back,0);//PWM比例0~255调速
+  delay(time * 100);	//执行时间，可以调整  
+}
+
+//急左转(左轮后退，右轮前进)
+void spin_left(int time)         
+{
+  digitalWrite(Right_motor_go,HIGH);	// 右电机前进
+  digitalWrite(Right_motor_back,LOW);
+  analogWrite(Right_motor_go,200); 
+  analogWrite(Right_motor_back,0);//PWM比例0~255调速
+  digitalWrite(Left_motor_go,LOW);   //左轮后退
+  digitalWrite(Left_motor_back,HIGH);
+  analogWrite(Left_motor_go,0); 
+  analogWrite(Left_motor_back,200);//PWM比例0~255调速
+  delay(time * 100);	//执行时间，可以调整  
+}
+
+//右转(右轮不动，左轮前进)
+void right(int time)        
+{
+  digitalWrite(Right_motor_go,LOW);   //右电机不动
+  digitalWrite(Right_motor_back,LOW);
+  analogWrite(Right_motor_go,0); 
+  analogWrite(Right_motor_back,0);//PWM比例0~255调速
+  digitalWrite(Left_motor_go,HIGH);//左电机前进
+  digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,200); 
+  analogWrite(Left_motor_back,0);//PWM比例0~255调速
+  delay(time * 100);	//执行时间，可以调整  
+}
+
+//急右转(右轮后退，左轮前进)
+void spin_right(int time)        
+{
+  digitalWrite(Right_motor_go,LOW);   //右电机后退
+  digitalWrite(Right_motor_back,HIGH);
+  analogWrite(Right_motor_go,0); 
+  analogWrite(Right_motor_back,200);//PWM比例0~255调速
+  digitalWrite(Left_motor_go,HIGH);//左电机前进
+  digitalWrite(Left_motor_back,LOW);
+  analogWrite(Left_motor_go,200); 
+  analogWrite(Left_motor_back,0);//PWM比例0~255调速
+  delay(time * 100);	//执行时间，可以调整  
+}
+
+void back(int time)          //倒车
+{
+  digitalWrite(Right_motor_go,LOW);  //右轮后退
+  digitalWrite(Right_motor_back,HIGH);
+  analogWrite(Right_motor_go,0);
+  analogWrite(Right_motor_back,150);//PWM比例0~255调速
+  digitalWrite(Left_motor_go,LOW);  //左轮后退
+  digitalWrite(Left_motor_back,HIGH);
+  analogWrite(Left_motor_go,0);
+  analogWrite(Left_motor_back,150);//PWM比例0~255调速
+}
+
+
