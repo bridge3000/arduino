22b7ca0ae2589c7812be8de3aca0e2701a278a92
bridge3000 <9455856@qq.com>
2016-05-14 00:38:44 +0800

    sound

--- sound_control_light/sound_control_light.ino
+++ sound_control_light/sound_control_light.ino
@@ -5,21 +5,26 @@ IDE 版本:1.0.1
 功能：模拟声控灯开关状态，当音量超过设定值后，点亮LED，延时一段时间后熄灭
 */
 int LEDpin = 13;                 //定义LED灯控制引脚
+int soundpin = 8;
 
 void setup() {
+  Serial.begin(9600);//设置波特率为9600
+  Serial.println("init");
   pinMode(LEDpin, OUTPUT);
+  pinMode(soundpin, INPUT);
+
 }
 
 void loop() {
-  int Soundvalue = analogRead(A0);  //读取输入模拟值
-  if (Soundvalue > 20)
+  int Soundvalue = digitalRead(soundpin);  //读取输入模拟值
+  Serial.println( digitalRead(soundpin));
+  if (Soundvalue == 0)
   {
     digitalWrite(LEDpin, HIGH);     //当模拟值大于设定值后，点亮LED
-    for (int i = 0; i < 20; i++) {
-      delay(1000);                    //延时20s
-    }
+    delay(20000);                    //延时20s
   }
-  else {
+  else 
+  {
     digitalWrite(LEDpin, LOW);     //关闭LED
   }
 }
